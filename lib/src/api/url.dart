40420ae2baf7ed1d8c4d8e9708e988b30307fd3b
Marcelo Manjabosco [SBsistemas] <marcelo@sbsistemas.com.br>
2022-01-27 16:12:36 -0300

    feat: implementa base de api

---
+++ lib/src/api/url.dart
@@ -0,0 +1,10 @@
+class Url {
+  static const String baseURL = 'https://api.themoviedb.org/3';
+  static const String baseURLImage = 'https://image.tmdb.org/t/p/w500';
+
+  static const String movie = '/movie';
+  static String getImageUrl(String path) => baseURLImage + path;
+  static const String popularMovies = movie + '/popular';
+  static String search(String query) => "/search" + movie;
+  static const String genre = '/genre/movie/list';
+}
