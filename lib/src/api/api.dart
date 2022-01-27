bf1f7b459a8acf7b4c63f041a1e135c1ec651c33
Marcelo Manjabosco [SBsistemas] <marcelo@sbsistemas.com.br>
2022-01-27 16:12:43 -0300

    feat: implementa base de api

---
+++ lib/src/api/api.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_thmdb_app/src/api/dio/dio_client.dart';
+import 'package:flutter_thmdb_app/src/api/url.dart';
+
+//configurações referente a api.
+const String apiKey = 'e3db5bb0c72d2420dc98511d974049de';
+
+class Api {
+  static CustomDio instanceDio;
+
+  static CustomDio DioClient() {
+    instanceDio ??= CustomDio(
+      con: Dio(),
+      baseUrl: Url.baseURL,
+    );
+
+    return instanceDio;
+  }
+
+  static Future fetchMovieList({String query, int page}) async {
+    Response res;
+    if (query == null || query.isEmpty) {
+      res = await DioClient().con.get(Url.popularMovies);
+    } else {
+      res = await DioClient()
+          .con
+          .get(Url.search(query), queryParameters: {'query': query});
+    }
+
+    return res;
+  }
+
+  static Future fetchMovieInfo(int movieID) async =>
+      await DioClient().con.get(Url.movie + "/$movieID");
+
+  static Future fetchGenre() async => await DioClient().con.get(Url.genre);
+}
