9731a83cc7fc1b5b65a3bfc7e4203f4747a23651
Marcelo <marcelo@marcelo.com>
2022-01-30 16:22:17 -0300

    feat: implementa componente (botão voltar)

---
+++ lib/src/components/back_button/back_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_thmdb_app/src/shared/consts.dart';
+import 'package:flutter_thmdb_app/src/shared/utils/typography/typography.dart';
+
+class MBackButton extends StatelessWidget {
+  MBackButton(
+      {this.textTranslation,
+      this.controller,
+      this.buttonTranslation,
+      this.callback});
+
+  final Animation<Offset> textTranslation;
+  final Animation<Offset> buttonTranslation;
+  final Animation<double> controller;
+  final VoidCallback callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: callback,
+      child: AnimatedBuilder(
+        animation: controller,
+        builder: (context, child) {
+          return FractionalTranslation(
+            translation: buttonTranslation.value,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Icon(
+                  Icons.arrow_back_ios,
+                  color: gray02,
+                  size: 14,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                FractionalTranslation(
+                    translation: textTranslation.value,
+                    child: CustomTypography.title14('Voltar',
+                        color: gray02, fontWeight: FontWeight.w500)),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
