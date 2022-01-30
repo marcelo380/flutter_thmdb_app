1ed51f9dc2c0c48d6f2037ad050edbb0a8e4f950
Marcelo <marcelo@marcelo.com>
2022-01-30 16:24:14 -0300

    refactor: ajusta padroes da typography

--- lib/src/shared/utils/typography/typography.dart
+++ lib/src/shared/utils/typography/typography.dart
@@ -3,7 +3,15 @@ import 'package:flutter_thmdb_app/src/shared/consts.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomTypography {
-  
+  static Text title24(String content,
+          {Color color = gray01,
+          TextOverflow overFlow,
+          TextAlign textAlign,
+          FontStyle fontStyle,
+          double height}) =>
+      _makeTextComponent(content, 18.0, FontWeight.w600,
+          color: color, overFlow: overFlow, align: textAlign, height: height);
+
   static Text title18(String content,
           {Color color = gray01,
           TextOverflow overFlow,
@@ -13,13 +21,23 @@ class CustomTypography {
       _makeTextComponent(content, 18.0, FontWeight.w600,
           color: color, overFlow: overFlow, align: textAlign, height: height);
 
+  static Text title12(String content,
+          {Color color = gray01,
+          TextOverflow overFlow,
+          TextAlign textAlign,
+          FontStyle fontStyle,
+          double height}) =>
+      _makeTextComponent(content, 12.0, FontWeight.w600,
+          color: color, overFlow: overFlow, align: textAlign, height: height);
+
   static Text title14(String content,
           {Color color = Colors.white,
           TextOverflow overFlow,
           TextAlign textAlign = TextAlign.left,
           FontStyle fontStyle,
+          FontWeight fontWeight = FontWeight.w600,
           double height}) =>
-      _makeTextComponent(content, 14.0, FontWeight.w600,
+      _makeTextComponent(content, 14.0, fontWeight,
           color: color, overFlow: overFlow, align: textAlign, height: height);
 
   static Text body10(String content,
@@ -27,8 +45,19 @@ class CustomTypography {
           TextOverflow overFlow,
           TextAlign textAlign = TextAlign.left,
           FontStyle fontStyle,
+          FontWeight fontWeight = FontWeight.w400,
           double height}) =>
-      _makeTextComponent(content, 14.0, FontWeight.w400,
+      _makeTextComponent(content, 10.0, fontWeight,
+          color: color, overFlow: overFlow, align: textAlign, height: height);
+
+  static Text body14(String content,
+          {Color color,
+          TextOverflow overFlow,
+          TextAlign textAlign,
+          FontStyle fontStyle,
+          FontWeight fontWeight = FontWeight.w600,
+          double height}) =>
+      _makeTextComponent(content, 14.0, fontWeight,
           color: color, overFlow: overFlow, align: textAlign, height: height);
 
   static Text bodySmall(String content,
