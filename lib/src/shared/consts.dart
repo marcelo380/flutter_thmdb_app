bf5aa32ab4b4a64353c8d731407f2bc7baa96d80
Marcelo <marcelo@marcelo.com>
2022-01-31 19:33:58 -0300

    refactor: adiciona generos para uso offline (somente para ex)

--- lib/src/shared/consts.dart
+++ lib/src/shared/consts.dart
@@ -6,3 +6,25 @@ const Color gray03 = Color(0xff868e96);
 const Color gray08 = Color(0xFFF1F3F5);
 const Color darkBlue = Color(0xFF00384C);
 const Color grayWhite = Color(0xFFFFFFFF);
+
+const generosOfline = [
+  {"id": 28, "name": "Ação"},
+  {"id": 12, "name": "Aventura"},
+  {"id": 16, "name": "Animação"},
+  {"id": 35, "name": "Comédia"},
+  {"id": 80, "name": "Crime"},
+  {"id": 99, "name": "Documentário"},
+  {"id": 18, "name": "Drama"},
+  {"id": 10751, "name": "Família"},
+  {"id": 14, "name": "Fantasia"},
+  {"id": 36, "name": "História"},
+  {"id": 27, "name": "Terror"},
+  {"id": 10402, "name": "Música"},
+  {"id": 9648, "name": "Mistério"},
+  {"id": 10749, "name": "Romance"},
+  {"id": 878, "name": "Ficção científica"},
+  {"id": 10770, "name": "Cinema TV"},
+  {"id": 53, "name": "Thriller"},
+  {"id": 10752, "name": "Guerra"},
+  {"id": 37, "name": "Faroeste"}
+];
