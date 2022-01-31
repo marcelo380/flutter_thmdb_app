63e028bdc011507c1399c12e3f9707de73a3af30
Marcelo <marcelo@marcelo.com>
2022-01-31 19:37:20 -0300

    refactor: adiciona ensureInitialized no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_thmdb_app/src/pages/home_page/home_page.dart';
 import 'package:flutter_thmdb_app/src/shared/singleton/genre_singleton.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GenreSingleton().init();
+
   runApp(MyApp());
 }
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-   //builder: RequestLoader.initLoading(context),
+      //builder: RequestLoader.initLoading(context),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
