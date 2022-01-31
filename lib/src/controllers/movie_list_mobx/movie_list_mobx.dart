fcdb2d99d238268565601bae04565c88227b4889
Marcelo <marcelo@marcelo.com>
2022-01-31 19:31:44 -0300

    refactor: adiciona sistema para gerenciamento do cache offline

--- lib/src/controllers/movie_list_mobx/movie_list_mobx.dart
+++ lib/src/controllers/movie_list_mobx/movie_list_mobx.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_thmdb_app/src/api/api.dart';
 import 'package:flutter_thmdb_app/src/models/movie_list_model.dart';
+import 'package:flutter_thmdb_app/src/shared/utils/connectivity/connectivity.dart';
+import 'package:flutter_thmdb_app/src/storage/cache/cache_movie_list.dart';
+
 import 'package:mobx/mobx.dart';
 
 part 'movie_list_mobx.g.dart';
@@ -8,7 +11,7 @@ part 'movie_list_mobx.g.dart';
 class MoviesMobxCTRL = _MoviesMobxCTRLBase with _$MoviesMobxCTRL;
 
 abstract class _MoviesMobxCTRLBase with Store {
-  MovieListModel movieListModel = MovieListModel();
+  MovieListModel _movieListModel = MovieListModel();
 
   @observable
   ObservableList<ResultMovie> movieList = ObservableList();
@@ -41,32 +44,49 @@ abstract class _MoviesMobxCTRLBase with Store {
     int forcePage,
     String query,
   }) async {
-    if (page == 1) {
-      setLoading(true);
-    }
-
-    paging = true;
-    if (forcePage != null) {
-      page = forcePage;
-    }
-
-    await Api.fetchMovieList(
-      query: query,
-      page: page,
-    ).then((response) async {
+    //verificar conexao
+    if (await CheckConnectivity.hasConection()) {
       if (page == 1) {
-        movieListModel = MovieListModel.fromMap(response.data);
-      } else {
-        movieListModel.results
-            .addAll(MovieListModel.fromMap(response.data).results);
+        setLoading(true);
       }
 
-      movieList.clear();
-      movieList.addAll(movieListModel.results);
-      paging = false;
-    });
-    if (page == 1) {
-      setLoading(false);
+      paging = true;
+      if (forcePage != null) {
+        page = forcePage;
+      }
+
+      await Api.fetchMovieList(
+        query: query,
+        page: page,
+      ).then((response) async {
+        if (page == 1) {
+          _movieListModel = MovieListModel.fromMap(response.data);
+          //guarda dados para uso offline
+          await CacheMovieList.insertMovieList(_movieListModel.results);
+        } else {
+          _movieListModel.results
+              .addAll(MovieListModel.fromMap(response.data).results);
+          //para evitar fica jogando lista completa
+          await CacheMovieList.insertMovieList(
+              MovieListModel.fromMap(response.data).results);
+        }
+
+        movieList.clear();
+        movieList.addAll(_movieListModel.results);
+
+        paging = false;
+      });
+      if (page == 1) {
+        setLoading(false);
+      }
+
+      //tenta carregar dados offline
+    } else {
+      List<ResultMovie> movieOfflineList = await CacheMovieList.readMovieList();
+
+      if (movieOfflineList != null || movieList.isNotEmpty) {
+        movieList.addAll(movieOfflineList);
+      }
     }
   }
 }
