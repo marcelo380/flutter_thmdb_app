a8d85ccd096e520768d5be33fe3ae11e038defeb
Marcelo <marcelo@marcelo.com>
2022-01-30 22:48:15 -0300

    refactor: adiciona nova cor

--- lib/src/shared/consts.dart
+++ lib/src/shared/consts.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 const Color gray01 = Color(0xFF343A40);
 const Color gray02 = Color(0xFF5E6770);
+const Color gray03 = Color(0xff868e96);
 const Color gray08 = Color(0xFFF1F3F5);
 const Color darkBlue = Color(0xFF00384C);
 const Color grayWhite = Color(0xFFFFFFFF);
